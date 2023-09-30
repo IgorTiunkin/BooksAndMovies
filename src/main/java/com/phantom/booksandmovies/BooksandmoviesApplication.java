74856d4dd3b82eaf86460aee9a942e9e5135aa9f
Igor Tiunkin <fantomenergy@gmail.com>
2023-09-30 14:40:18 +0300

    Base structure for project

---
+++ src/main/java/com/phantom/booksandmovies/BooksandmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.phantom.booksandmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksandmoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksandmoviesApplication.class, args);
+    }
+
+}
